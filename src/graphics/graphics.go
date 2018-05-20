8c72e144da16454c0c982223904cc2451f9a4441
rog-works <rog_works@yahoo.co.jp>
2018-05-20 10:21:41 +0900

    feat: update graphics

--- src/graphics/graphics.go
+++ src/graphics/graphics.go
@@ -1,33 +1,57 @@
-package main
+package graphics
 
 import "math"
 
-type Screen struct {
-	bytes []byte
+type Pixel struct {
+	buf byte
 }
 
-func (s *Screen) Init(width, height int) {
-	s.bytes = make(byte, width * height)
+type Screen struct {
+	pixels []Pixel
+	Width int
+	Height int
+}
+
+type Image Screen
+
+func New(width, height int) (*Screen) {
+	return &Screen{
+		make(Pixel, width * height),
+		width,
+		height
+	}
 }
 
 type Graphics struct {
 	screen Screen
 }
 
-func (g *Graphics) DrawString(str string) {
-	
+func (g *Graphics) DrawPixel(pixel Pixel, offsetX, offsetY int) {
+	if offsetX < 0 || offsetY < 0 {
+		continue
+	}
+	if g.scree.Width < offsetX || g.scree.Height < offsetY {
+		continue
+	}
+	var index := offsetY * g.screen.Width + offsetX
+	g.screen[index] = pixel
 }
 
 func (g *Graphics) DrawImage(image *Image, offsetX, offsetY int) {
 	for var x := 0; x < image.Width; x++ {
 		for var y := 0; y < image.Height; y++ {
-			if x + offsetX < 0 || y + offsetY < 0 {
-				continue
-			}
-			if image.Width < x + offsetX || image.Height < y + offsetY {
-				continue
-			}
-			g.DrawPixel(image.pixelAt(x, y), x + offsetX, y + offsetY)
+			g.DrawPixel(image.PixelAt(x, y), x + offsetX, y + offsetY)
 		}
 	}
 }
+
+func (g *Graphics) DrawChar(char string, offsetX, offsetY int) {
+	g.DrawPixel(new Pixel(char), offsetX, offsetY)
+}
+
+func (g *Graphics) DrawString(str string, offsetX, offsetY) {
+	for var i := 0; i < str.Length; i++ {
+		g.DrawChar(str[i], offsetX, offsetY)
+	}
+}
+
